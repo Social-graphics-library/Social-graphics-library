c703887472b116438a3fcc973782b96f72e8432c
JosunLP <jolun.swtor@gmail.com>
2020-11-02 20:19:18 +0100

    adding virtual dom

--- tests/index.js
+++ tests/index.js
@@ -1,1 +1,29 @@
-console.log("No tests at this moment...");
+const jsdom = require("jsdom");
+// const { SocialGraphicsLibrary } = require("../dist/sgl.bundle");
+
+//#region setup
+const { JSDOM } = jsdom;
+const dom = new JSDOM(`<!DOCTYPE html><body id="body"></body>`, {
+    url: "https://example.org/",
+    referrer: "https://example.com/",
+    contentType: "text/html",
+    includeNodeLocations: true,
+    storageQuota: 10000000
+});
+const doc = dom.window.document;
+const main = doc.createElement("div");
+const testDiv = doc.createElement("div");
+
+main.id = "main";
+testDiv.id = "test";
+testDiv.className = "test";
+
+doc.getElementById("body").appendChild(main);
+doc.getElementById("main").appendChild(testDiv);
+//#endregion
+
+//#region test
+//#endregion
+
+
+console.log("Tests done...");
