44c0c644a263be28290cb503fa07a1cec802b092
JosunLP <jolun.swtor@gmail.com>
2020-12-05 13:51:02 +0100

    Deploy website - based on 1c621479e71334d7002f8fb13ffe210c64c7a1c1

--- example.js
+++ example.js
@@ -2,6 +2,20 @@ import "./dist/sgl.bundle.js";
 
 var sgl = new SocialGraphicsLibrary();
 
+docReady(function() {
+    sgl.multiGenerator(document.getElementById("tName").value, document.getElementById("uName").value, [{
+        mode: "logo",
+        containerId: "img-container-1",
+        imgMode: "webp",
+        generateLink: true
+    }, {
+        mode: "twitch-title",
+        containerId: "img-container-2",
+        imgMode: "jpeg",
+        generateLink: true
+    }])
+})
+
 document.getElementById("generate").addEventListener("click", async function() {
     sgl.multiGenerator(document.getElementById("tName").value, document.getElementById("uName").value, [{
         mode: "logo",
@@ -15,3 +29,13 @@ document.getElementById("generate").addEventListener("click", async function() {
         generateLink: true
     }])
 });
+
+function docReady(fn) {
+    // see if DOM is already available
+    if (document.readyState === "complete" || document.readyState === "interactive") {
+        // call on next available tick
+        setTimeout(fn, 1);
+    } else {
+        document.addEventListener("DOMContentLoaded", fn);
+    }
+}
