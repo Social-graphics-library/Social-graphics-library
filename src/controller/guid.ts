ceed85c915a0fdf22425ccab8019e0195703db98
Jonas Pfalzgraf <jolun.swtor@gmail.com>
2022-08-31 21:22:25 +0200

    Fixing Stuff

--- src/controller/guid.ts
+++ src/controller/guid.ts
@@ -2,9 +2,14 @@
  * Generates a new GUID string.
  */
 export class Guid {
-
+	/**
+	 * Guid  of guid
+	 */
 	private guid: string;
 
+	/**
+	 * Creates an instance of guid.
+	 */
 	constructor() {
 		this.guid = Guid.newGuid();
 	}
@@ -14,12 +19,20 @@ export class Guid {
 	 * @returns Guid
 	 */
 	private static newGuid() {
-		return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-			const r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8);
-			return v.toString(16);
-		});
+		return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(
+			/[xy]/g,
+			function (c) {
+				const r = (Math.random() * 16) | 0,
+					v = c === "x" ? r : (r & 0x3) | 0x8;
+				return v.toString(16);
+			}
+		);
 	}
 
+	/**
+	 * To string
+	 * @returns
+	 */
 	public toString() {
 		return this.guid;
 	}
--- src/controller/stringCleaner.ts
+++ src/controller/stringCleaner.ts
@@ -2,14 +2,12 @@
  * Image renderer
  */
 export class StringCleaner {
-
 	/**
 	 * Runs string cleaner
 	 * @param item
 	 * @returns run
 	 */
 	public static run(item: string): string {
-
 		item = item.replace(/[^a-zA-Z0-9\_\-\.\~]/g, "");
 
 		return item;
